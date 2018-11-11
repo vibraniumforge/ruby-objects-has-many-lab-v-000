d9155004cd6b8019050717bea36dad95b999494c
Greg Morgan <gregm9999@gmail.com>
2018-11-11 00:24:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,28 @@
+require "pry"
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
-  @@all = []
-
+  @@song_count=0
   def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
+    @name=name
+    @songs=[]
   end
 
   def add_song(song)
-    song.artist = self
+    @songs<<song
+    song.artist=self
+    @@song_count+=1
   end
 
   def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end
-
-  def songs
-    Song.all.select {|song| song.artist == self}
+    song=Song.new(name)
+    @songs<<song
+    song.artist=self
+    @@song_count+=1
   end
 
   def self.song_count
-    Song.all.count
+    @@song_count
   end
+
 end
